16dc4deaa03bcafc0eefdd2a3c7972cac612a155
Ilya Maximov <mail@overlisted.net>
2020-11-06 16:34:06 +0100

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+function readFileString(path) {
+  return fs.readFileSync(path, {encoding: "utf8"}).replace(/\r?\n|\r/g, "");
+}
+
+const token = readFileString("token.txt");
+const channel = readFileString("channel.txt");
+
+const headers = {authorization: token};
+
+async function request(before) {
+  const options = {
+    method: "GET",
+    headers: headers
+  };
+  
+  const request = await fetch(
+    `https://discord.com/api/channels/${channel}/messages?limit=100&${before ? "before=" + before : ""}`, 
+    options
+  );
+    
+  return await request.json();
+}
+
+let result;
+
+async function go() {
+  let page = await request();
+  
+  result = page;
+
+  while(page.length >= 100) {
+    page = await request(page[page.length - 1].id);
+    result = result.concat(page);
+  }
+  
+  console.log(`Fetched ${result.length} messages`);
+  
+  fs.writeFileSync("messages.json", JSON.stringify(result, null, 2));
+}
+
+go();
